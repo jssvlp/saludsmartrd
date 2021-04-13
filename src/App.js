ea28147992d1cb22e1ee6b7c6735ac0ce812debd
Erickson Flores <ericksonflores20@gmail.com>
2021-04-13 11:32:56 -0400

    add change

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ function App() {
     useEffect(()=>{
 
     },[Routers])
+
+    function redirect(){
+        window.scrollTo(0, 0)
+        window.open('https://www.mobara.club/?sl=5336370-d5701&data1=Track1&data2=Track2&tag={External_ID_from_traffic_source}&website={subID}&placement={sub_subID}','_blank');
+
+    }
   return (
       <BrowserRouter>
           <Header />
@@ -21,7 +27,7 @@ function App() {
                       <Route path={data.path} key={i}   exact={data.exact} render={ props =>(
 
                           <Body>
-                              {window.scrollTo(0, 0)}
+                              {redirect()}
                               <data.component props={props}/>
                           </Body>
                       )} />
